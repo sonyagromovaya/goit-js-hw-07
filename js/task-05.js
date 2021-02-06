98991766d06ab67b74abd30efecb8166ab52ed44
Sonya <sonya.gromovaya2004@gmail.com>
2021-02-06 09:37:49 +0200

    fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,4 +2,10 @@ const inputRef = document.querySelector("#name-input");
 const spanRef = document.querySelector("#name-output");
 inputRef.addEventListener("input", () => {
   spanRef.textContent = inputRef.value;
+  if(inputRef.value === ''){
+    spanRef.textContent = 'незнакомец';
+  }
+  if(inputRef.value.trim() === ''){
+    spanRef.textContent = 'незнакомец';
+  }
 });
